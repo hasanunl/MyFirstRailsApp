6126ae3d29f0f965605dc2f33cbbe2b479c3d4ce
hasanunl <hu1997tr@gmail.com>
2018-06-22 16:11:36 +0300

    Completed application.

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  resources :articles
+  root 'welcome#index'
+  resources :high_scores
+  get 'greetings/hello'
+  resources :articles do
+    resources :comments
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
